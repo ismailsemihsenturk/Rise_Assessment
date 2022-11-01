52008182dc05668ba9f0549998243324d19e8b2b
İsmail Semih Şentürk <senturkis98@hotmail.com>
2022-11-01 16:35:32 +0300

    callback jobs

--- Frontend/rise_assessment/src/components/filterJobs/FilterJob.jsx
+++ Frontend/rise_assessment/src/components/filterJobs/FilterJob.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import "./FilterJob.css";
 
 
-export default function FilterJob() {
+export default function FilterJob(props) {
   return (
     <div>Filter Job</div>
   )

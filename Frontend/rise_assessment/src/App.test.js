e9adce91b227df673592ccd03fcbcd29317d2adf
İsmail Semih Şentürk <senturkis98@hotmail.com>
2022-11-03 00:42:32 +0300

    entagration tests

--- Frontend/rise_assessment/src/App.test.js
+++ Frontend/rise_assessment/src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from "./App.js"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Job", () => {
+
+  it('job adding test', async () => {
+    render(
+      <App />
+    );
+
+    const inputElement = screen.getByTestId("input-job-name");
+    const selectElement = screen.getByTestId("input-job-priority");
+    const buttonElement = screen.getByTestId("button-job-create");
+    fireEvent.change(inputElement, { target: { value: "testing the validation" } });
+    fireEvent.change(selectElement, { target: { value: "Urgent" } });
+    fireEvent.click(buttonElement);
+    const JobElement = screen.getByText("testing the validation");
+    expect(JobElement).toBeInTheDocument();
+
+
+  });
+
+  it('job filter test', () => {
+    render(
+      <App />
+    );
+    const inputElement = screen.getByTestId("filter-job-name");
+    fireEvent.change(inputElement, { target: { value: "testing the filtering" } });
+    const filterElement = screen.getByText("testing the filtering");
+    expect(filterElement).toBeInTheDocument();
+  });
 });
